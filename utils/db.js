fc3f367ccd5d69c9a54888f133387812177b8b53
thanhnam <thanhnam@example.com>
2026-08-20 18:13:31 +0700

    fix

--- utils/db.js
+++ utils/db.js
@@ -5,7 +5,7 @@ const {
   MYSQL_HOST = '192.168.2.52',
   MYSQL_PORT = 3306,
   MYSQL_USER = 'root',
-  MYSQL_PASSWORD = '',
+  MYSQL_PASSWORD = 'Nguyen20004nam@',
   MYSQL_DATABASE = 'zalo_data'
 } = process.env;
 
